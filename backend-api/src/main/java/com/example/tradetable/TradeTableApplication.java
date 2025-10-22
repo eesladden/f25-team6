ebe6dae15da4dc5ef2dc0a349fe812069060264a
Emma Sladden <eesladden@uncg.edu>
2025-10-22 12:28:45 -0400

    Add scaffolding files from Spring Initializr to subfolder

---
+++ backend-api/src/main/java/com/example/tradetable/TradeTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeTableApplication.class, args);
+	}
+
+}
